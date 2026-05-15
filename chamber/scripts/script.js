055490b0c1aa0cc771403e37ce779ee9e1ced7d9
Helamã Y. Barbour <helamashi@gmail.com>
2026-05-15 10:32:50 -0300

    Created the html for the chamber project.

---
+++ chamber/scripts/script.js
@@ -0,0 +1,24 @@
+/**
+ * Script to update the current year and last modified date on the webpage.
+ */
+const date = new Date();
+const year = date.getFullYear();
+
+document.getElementById("currentYear").textContent = year;
+document.getElementById("lastModified").textContent = `Last modification: ${document.lastModified}`;
+
+/**
+ * This script is responsable to show or hide the navigation menu.
+ */
+
+const dropdownBtn = document.getElementById("dropdownImg");
+const nav = document.querySelector("header nav ul");
+
+dropdownBtn.addEventListener("click", () => {
+    if(nav.style.display == "") {
+        nav.style.display = "flex";
+    }
+    else if(nav.style.display == "flex") {
+        nav.style.display = "";
+    }
+})
