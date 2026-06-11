f5af1aebd0a6ec02e03de5417754c28f40f125a7
Helamã Y. Barbour <90625373+helaman-Y-B@users.noreply.github.com>
2026-06-11 22:40:47 +0000

    added transition for the modals when they close and open.
    It changes the opacity.

--- shareHike/scripts/become-modal.mjs
+++ shareHike/scripts/become-modal.mjs
@@ -17,27 +17,41 @@ export default function controlModal() {
     // Events Listeners to open modals
     freeInput.addEventListener("click", () => {
         freeModal.showModal();
+        freeModal.classList.add("show");
     })
 
     bronzeInput.addEventListener("click", () => {
         bronzeModal.showModal();
+        bronzeModal.classList.add("show");
     })
 
     silverInput.addEventListener("click", () => {
         silverModal.showModal();
+        silverModal.classList.add("show");
     })
 
     goldInput.addEventListener("click", () => {
         goldModal.showModal();
+        goldModal.classList.add("show");
     })
 
     // Events Listeners to close modals
     closeBtns.forEach(btn => {
         btn.addEventListener("click", () => {
-            freeModal.close();
-            bronzeModal.close();
-            silverModal.close();
-            goldModal.close();
+
+            freeModal.classList.remove("show");
+            setTimeout(() => freeModal.close(), 600);
+
+            bronzeModal.classList.remove("show");
+            setTimeout(() => bronzeModal.close(), 600);
+
+            silverModal.classList.remove("show");
+            setTimeout(() => silverModal.close(), 600);
+
+            goldModal.classList.remove("show");
+            setTimeout(() => goldModal.close(), 600);
+
+
         })
     });
 
