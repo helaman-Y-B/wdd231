cdc16456e4fe8ed8aad4a16c589a7b3e20769baa
Helamã Y. Barbour <helamashi@gmail.com>
2026-06-13 02:01:42 -0300

    Created the lowest rating to hights rating sort button.

--- shareHike/scripts/controller-hikes.mjs
+++ shareHike/scripts/controller-hikes.mjs
@@ -1,9 +1,15 @@
-import { displayHikes, displayHighToLow } from "./show-hikes.mjs";
+import { displayHikes, displayHighToLow, displayLowToHigh } from "./show-hikes.mjs";
 
-const highToLowBtn = document.querySelector("#highToLow")
+const highToLowBtn = document.querySelector("#highToLow");
 
 highToLowBtn.addEventListener("click", () => {
     displayHighToLow();
+})
+
+const lowToHighBtn = document.querySelector("#lowToHigh");
+
+lowToHighBtn.addEventListener("click", () => {
+    displayLowToHigh();
 })
 
 displayHikes();
--- shareHike/scripts/show-hikes.mjs
+++ shareHike/scripts/show-hikes.mjs
@@ -143,3 +143,48 @@ export async function displayHighToLow() {
     }
     
 }
+
+export async function displayLowToHigh() {
+    try {
+        let html = "";
+        const section = document.querySelector("#hikes");
+        
+        const data = await getData();
+
+        // Sort data ascending (Lowest to Highest) based on the string length of the stars in rating.
+        const sortedData = data.sort((a, b) => a.rating.length - b.rating.length);
+
+        sortedData.forEach(hike => {
+            
+            html += `
+                <div class="hike">
+                    <h2>By ${hike.user}</h2>
+
+                    <img src="${hike.img}"
+                        alt="${hike.name}" loading="lazy">
+
+                    <p id="rating">${hike.rating}</p>
+                    
+                    <h3>${hike.title}</h3>
+
+                    <p>${hike.description}</p>
+
+                    <p>Location: ${hike.location}</p>
+                    <div class="category">
+                        <h4>Categories</h4>
+                        <ul class="categories">
+                            ${hike.category.map(category => `<li>${category}</li>`).join('')}
+                        </ul>
+                    </div>
+                </div>
+            `
+        
+            
+        });
+
+        section.innerHTML = html;
+
+    } catch(error) {
+        console.log(`Failed to display Hikes: ${error}`);
+    }
+}
